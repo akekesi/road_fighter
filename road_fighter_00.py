29f840e76081580cf8ab46c96472e51fb0e840e2
KEKESATI <KEKESATI@schaeffler.com>
2023-08-28 23:25:35 +0200

    updated look, added +- score, after_cancels do not work

--- road_fighter_00.py
+++ road_fighter_00.py
@@ -8,6 +8,11 @@ class RoadFighter(customtkinter.CTk):
     speed = 1
     time = time.time()
     score = 0
+    score_pos = "+"
+    score_neg = "-"
+
+    after_id_game = None
+    after_id_time = None
 
     road_l = list("XX|")
     road_r = copy.deepcopy(road_l)
@@ -19,7 +24,8 @@ class RoadFighter(customtkinter.CTk):
     for i in range(10):
         road.append(copy.deepcopy(road_line))
 
-    car = len(road_l) + len(road_m) - 1
+    car_idx = len(road_l) + len(road_m) - 1
+    car = "↑"
 
     padx_grid = 10
     pady_grid = 10
@@ -32,15 +38,26 @@ class RoadFighter(customtkinter.CTk):
         self.resizable(0,0)
 
         # frame - textbox
-        self.frame_textbox = customtkinter.CTkFrame(self)
+        self.frame_textbox = customtkinter.CTkFrame(self, fg_color="transparent")
         self.frame_textbox.grid(row=0, column=0, padx=self.padx_grid, pady=(self.pady_grid, 0))
 
-        self.textbox = customtkinter.CTkTextbox(self.frame_textbox, width=105, height=190, activate_scrollbars=False, autoseparators=True, font=("Courier", 14), state="disabled")
+        self.textbox = customtkinter.CTkTextbox(self.frame_textbox, width=105, height=190, activate_scrollbars=False, autoseparators=True, font=("Courier", 14), state="disabled", border_width=2, border_color="#565B5E")
         self.textbox.grid(row=0, column=0, sticky="nsew")
 
+        # frame - info
+        self.frame_info = customtkinter.CTkFrame(self, fg_color="transparent")
+        self.frame_info.grid(row=1, column=0, padx=self.padx_grid, pady=(self.pady_grid, 0))
+
+        self.entry_speed = customtkinter.CTkEntry(self.frame_info, width=105, state="disabled", justify="right")
+        self.entry_speed.grid(row=0, column=0, padx=0, pady=0)
+        self.entry_time = customtkinter.CTkEntry(self.frame_info, width=105, state="disabled", justify="right")
+        self.entry_time.grid(row=1, column=0, padx=0, pady=1)
+        self.entry_score = customtkinter.CTkEntry(self.frame_info, width=105, state="disabled", justify="right")
+        self.entry_score.grid(row=2, column=0, padx=0, pady=0)
+
         # frame - buttons
-        self.frame_buttons = customtkinter.CTkFrame(self)
-        self.frame_buttons.grid(row=1, column=0, padx=self.padx_grid, pady=(self.pady_grid, 0))
+        self.frame_buttons = customtkinter.CTkFrame(self, fg_color="transparent")
+        self.frame_buttons.grid(row=2, column=0, padx=self.padx_grid, pady=self.pady_grid)
 
         self.button_up = customtkinter.CTkButton(self.frame_buttons, text="↑", width=35, command=lambda event=None: self.up(event))
         self.button_up.grid(row=0, column=1, sticky="ew")
@@ -50,24 +67,6 @@ class RoadFighter(customtkinter.CTk):
         self.button_right.grid(row=1, column=2, sticky="ew")
         self.button_down = customtkinter.CTkButton(self.frame_buttons, text="↓", width=35, command=lambda event=None: self.down(event))
         self.button_down.grid(row=2, column=1, sticky="ew")
-
-        # frame - info
-        self.frame_info = customtkinter.CTkFrame(self)
-        self.frame_info.grid(row=2, column=0, padx=self.padx_grid, pady=self.pady_grid)
-
-        self.label_speed = customtkinter.CTkLabel(self.frame_info, text="Speed:")
-        self.label_speed.grid(row=0, column=0, padx=(self.padx_grid, 0), pady=(self.pady_grid, 0), sticky="w")
-        self.label_time = customtkinter.CTkLabel(self.frame_info, text="Time:")
-        self.label_time.grid(row=1, column=0, padx=(self.padx_grid, 0), pady=(self.pady_grid, 0), sticky="w")
-        self.label_score = customtkinter.CTkLabel(self.frame_info, text="Score:")
-        self.label_score.grid(row=2, column=0, padx=(self.padx_grid, 0), pady=self.pady_grid, sticky="w")
-
-        self.entry_speed = customtkinter.CTkEntry(self.frame_info, width=35, state="disabled")
-        self.entry_speed.grid(row=0, column=1, padx=self.padx_grid, pady=(self.pady_grid, 0))
-        self.entry_time = customtkinter.CTkEntry(self.frame_info, width=35, state="disabled")
-        self.entry_time.grid(row=1, column=1, padx=self.padx_grid, pady=(self.pady_grid, 0))
-        self.entry_score = customtkinter.CTkEntry(self.frame_info, width=35, state="disabled")
-        self.entry_score.grid(row=2, column=1, padx=self.padx_grid, pady=self.pady_grid)
 
         # click
         self.bind("<Up>", self.up)
@@ -85,58 +84,68 @@ class RoadFighter(customtkinter.CTk):
         self.move()
         self.show()
         delay = int(1000 / self.speed)
-        self.after(delay, self.game)
+        self.after_id_game = self.after(delay, self.game)
         print("game")
 
     def up(self, event):
-        self.speed = min(10, self.speed + 1)
+        self.speed = min(10, self.speed + 0.5)
         self.set_speed()
         print(f"up")
 
     def down(self, event):
-        self.speed = max(1, self.speed - 1)
+        self.speed = max(1, self.speed - 0.5)
         self.set_speed()
         print(f"down")
 
     def left(self, event):
-        car_pre = self.car
-        self.car = max(len(self.road_l), self.car - 1)
-        if self.road[8][self.car] == "O":
+        car_pre = self.car_idx
+        self.car_idx = max(len(self.road_l), self.car_idx - 1)
+        if self.road[8][self.car_idx] == self.score_pos:
             self.score += 1
             self.set_score()
+        if self.road[8][self.car_idx] == self.score_neg:
+            self.score -= 1
+            self.set_score()
         self.road[8][car_pre] = self.road_line[car_pre]
-        self.road[8][self.car] = "↑"
+        self.road[8][self.car_idx] = self.car
         self.show()
         print("left")
 
     def right(self, event):
-        car_pre = self.car
-        self.car = min(len(self.road_l) + len(self.road_m) - 1, self.car + 1)
-        if self.road[8][self.car] == "O":
+        car_pre = self.car_idx
+        self.car_idx = min(len(self.road_l) + len(self.road_m) - 1, self.car_idx + 1)
+        if self.road[8][self.car_idx] == self.score_pos:
             self.score += 1
             self.set_score()
+        if self.road[8][self.car_idx] == self.score_neg:
+            self.score -= 1
+            self.set_score()
         self.road[8][car_pre] = self.road_line[car_pre]
-        self.road[8][self.car] = "↑"
+        self.road[8][self.car_idx] = self.car
         self.show()
         print("right")
 
     def set_speed(self):
         self.entry_speed.configure(state="normal")
         self.entry_speed.delete(0, "end")
-        self.entry_speed.insert(0, str(self.speed))
+        self.entry_speed.insert(0, f"{int(20*self.speed)} km/h")
         self.entry_speed.configure(state="disabled")
 
     def set_time(self):
         self.entry_time.configure(state="normal")
         self.entry_time.delete(0, "end")
-        self.entry_time.insert(0, f"{(time.time() - self.time):.1f}")
-        self.after(100, self.set_time)
+        self.entry_time.insert(0, f"{(time.time() - self.time):.1f} sec")
+        self.after_id_time = self.after(100, self.set_time)
         self.entry_time.configure(state="disabled")
 
     def set_score(self):
+        # if self.score > 1:
+        #     self.after_cancel(self.after_id_game) # <-- does not work ?!?!?
+        #     self.after_cancel(self.after_id_time)
+        #     return
         self.entry_score.configure(state="normal")
         self.entry_score.delete(0, "end")
-        self.entry_score.insert(0, str(self.score))
+        self.entry_score.insert(0, f"{100*self.score} $")
         self.entry_score.configure(state="disabled")
 
     def show(self):
@@ -148,15 +157,21 @@ class RoadFighter(customtkinter.CTk):
         print("show")
 
     def move(self):
-        if self.road[7][self.car] == "O":
+        if self.road[7][self.car_idx] == self.score_pos:
             self.score += 1
+            self.set_score()
+        if self.road[7][self.car_idx] == self.score_neg:
+            self.score -= 1
             self.set_score()
         new = copy.deepcopy(self.road_line)
         if not (random.randint(1, 4) % 4):
-            new[random.randint(len(self.road_l), len(self.road_l) + len(self.road_m) - 1)] = "O"
+            if not (random.randint(1, 2) % 2):
+                new[random.randint(len(self.road_l), len(self.road_l) + len(self.road_m) - 1)] = self.score_pos
+            else:
+                new[random.randint(len(self.road_l), len(self.road_l) + len(self.road_m) - 1)] = self.score_neg
         self.road.insert(0, new)
-        self.road[8][self.car] = "↑"
-        self.road[9][self.car] = self.road_line[self.car]
+        self.road[8][self.car_idx] = self.car
+        self.road[9][self.car_idx] = self.road_line[self.car_idx]
         self.road.pop()
         print("move")
 
